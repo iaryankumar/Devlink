36b352ce0d73ff0816ef9d8a124d2430b2f8db02
iaryankumar <k.aryan5716@gmail.com>
2025-07-05 20:59:17 +0530

    commit1

--- frontend/extra.tst
+++ frontend/extra.tst
@@ -1,12 +1,9 @@
-{/* <div class="absolute top-6 left-6 text-sm text-white/70 tracking-wider">
-                        A Creative <span class="inline-block w-10 border-b border-white/70 align-middle ml-1"></span>
+{/* <div className='h-[100px] w-full border'>
+                        <DotLottieReact
+                            src='https://lottie.host/6197d2b3-0419-467a-a97a-587f490c0ee6/T4VYw7obCK.lottie'
+                            loop
+                            autoplay
+                            color='blue'
+                            className='h-[450px] w-full border'
+                        />
                     </div> */}
-
-                    {/* <h1 class="mt-12 text-4xl font-bold leading-snug">
-                        Digital Marketing
-                    </h1>
-                    <h2 class="text-2xl font-light mt-1">Agency</h2>
-
-                    <button class="mt-6 w-fit px-6 py-2 border border-white rounded-full hover:bg-white hover:text-orange-600 transition">
-                        Get Started →
-                    </button> */}
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,26 +1,21 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { DotLottieReact } from '@lottiefiles/dotlottie-react'
 
 const Hero = () => {
     return (
         <>
-            <div className='h-max w-full flex items-center justify-center gap-2'>
-                <div class="bg-gradient-to-br from-gray-800 via-black to-black rounded-b-[5px] w-full h-full text-white shadow-lg relative p-0.5 ">
-                    <Navbar />
-                    <div className='h-[calc(90vh-80px)] w-full text-white mt-2 md:pt-18 flex lg:items-start items-center '>
-                        <div className='md:h-[calc(100%-140px)] h-[max] sm:mb-0 mb-16 w-full md:ps-16 sm:px-12 px-7 font-[500] font-primary flex flex-col items-center md:justify-start justify-center '>
-                            <h1 className='lg:text-[6rem] sm:text-[4.9rem] text-[3.5rem] sm:leading-20 leading-16 text-center'>We Build <span className='text-teal-500'>Smart Apps</span></h1>
-                            <h1 className='lg:text-[5rem] sm:text-[3.5rem] text-[2.5rem] text-center'>& <span className='text-teal-500 text-center'>Modern Webistes</span></h1>
-                        </div> 
+            <div class="bg-gradient-to-br from-gray-800 via-black to-black rounded-b-[5px] w-full h-full text-white shadow-lg relative p-0.5 ">
+                <Navbar />
+                <div className='min-h-[500px] max-h-[600px] sm:h-[550px] h-[600px] relative'>
+                    <div className='h-full w-full flex flex-col items-center justify-center font-[550] font-secondary md:px-3 px-8 text-center'>
+                        <h1 className='xl:text-[6.5rem] lg:text-[5.8rem] md:text-[4.5rem] text-[4rem] metallicEffect lg:leading-28 leading-23 textt'>We Build Smart Apps</h1>
+                        <h1 className='xl:text-[4.5rem] text-[3rem] metallicEffect textt'>& Modern Wesites</h1>
                     </div>
+                    
                 </div>
             </div>
-            <div className='h-[60vh] w-full relative flex items-center justify-center'>
-                <div className=' h-[400px] w-[85%] absolute sm:top-[-140px] top-[-90px]'>
-                    <img src="./hero.jpg" className='h-full w-full rounded-2xl object-cover' />
-                    <div className='h-full w-full bg-gradient-to-br from-gray-800 via-black to-gray-800 absolute top-0 opacity-60 rounded-2xl'></div>
-                </div>
-            </div>
+
 
         </>
     )
